5a6e97a4e77332b065b44001b99f90508c295d67
Roger <roger920407@gmail.com>
2016-12-28 15:56:49 -0800

    delete unnecessary component, add bootstrap and jquery

---
+++ views/src/app/Home/home.component.ts
@@ -0,0 +1,18 @@
+﻿import {Component} from "@angular/core";
+import "jquery";
+//import $ from "jquery";
+declare var $: any;
+
+@Component({
+    selector: "web-header",
+    templateUrl: "app/Home/home.html",
+    styleUrls: ["app/Home/home.css"]
+}) 
+
+export class homeHeaderComponent {
+    title = "<Roger/Liao>";
+    description = "roger's website";
+    console() {
+        $("body").addClass("meme");
+    }
+}
--- views/src/app/app.module.ts
+++ views/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { homeHeaderComponent } from './Home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        homeHeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [
+        homeHeaderComponent
+    ]
 })
 export class AppModule { }
